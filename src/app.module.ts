692e9b828f7d6607dfacb53b31fec5200d1b36c6
Tahmid <tahmidfarhan12@gmail.com>
2024-01-07 00:15:56 +0600

    vercel deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { ContactModule } from './contact/contact.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { PhoneEntryModule } from './phone-entry/phone-entry.module';
+import { ConfigModule } from '@nestjs/config';
 
-const DB_URI = 'mongodb://127.0.0.1:27017/phonebook-fullstack';
+// const DB_URI = 'mongodb://127.0.0.1:27017/phonebook-fullstack';
 
 @Module({
-  imports: [ContactModule, MongooseModule.forRoot(DB_URI), AuthModule, PhoneEntryModule],
+  imports: [ConfigModule.forRoot(),ContactModule, MongooseModule.forRoot(process.env.DB_URI), AuthModule, PhoneEntryModule],
   controllers: [AppController],
   providers: [AppService],
 })
